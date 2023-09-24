2e036c72ee25f2601e18446fb11514bf73e9e3d5
Cesar Herrera <cesarherr321@gmail.com>
2023-09-24 17:26:09 -0300

    Add comment migration

--- db/migrate/20230924200510_create_comments.rb
+++ db/migrate/20230924200510_create_comments.rb
@@ -1,8 +1,12 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+      t.text :text, null: false
       t.timestamps
     end
+    add_foreign_key :comments, :users, column: :user_id
+    add_foreign_key :comments, :posts, column: :post_id
   end
 end
