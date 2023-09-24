b2366a844037ba88c6ced278b734a5cb79a5396b
Cesar Herrera <cesarherr321@gmail.com>
2023-09-24 17:26:26 -0300

    Add likes migration

--- db/migrate/20230924200516_create_likes.rb
+++ db/migrate/20230924200516_create_likes.rb
@@ -1,8 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
       t.timestamps
     end
+    add_foreign_key :likes, :users, column: :user_id
+    add_foreign_key :likes, :posts, column: :post_id
   end
 end
