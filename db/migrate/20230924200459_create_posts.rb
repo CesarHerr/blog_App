f984e0422e381a2a813f23dc8badaf9f0fac4fac
Cesar Herrera <cesarherr321@gmail.com>
2023-09-24 17:25:53 -0300

    Add post migration

--- db/migrate/20230924200459_create_posts.rb
+++ db/migrate/20230924200459_create_posts.rb
@@ -1,8 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.integer :author_id, null: false
+      t.string :title, null: false, limit: 50
+      t.text :text, null: false
+      t.integer :likes_count, null: false, default: 0
+      t.integer :comments_count, null: false, default: 0
       t.timestamps
     end
+
+    add_foreign_key :posts, :users, column: :author_id
   end
 end
