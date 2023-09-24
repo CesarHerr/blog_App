09687cd845bd342b955bd6741036b3a84a50dba5
Cesar Herrera <cesarherr321@gmail.com>
2023-09-24 17:25:37 -0300

    Add user migration

--- db/migrate/20230924200446_create_users.rb
+++ db/migrate/20230924200446_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false, limit: 50
+      t.binary :photo, null: false
+      t.text :bio, null: false
+      t.integer :posts_count, null: false, default: 0
       t.timestamps
     end
   end
